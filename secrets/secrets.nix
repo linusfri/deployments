a1ef1dc211f58fc0c9d8a3be975fbb48e0df397c
linus <linus.f@protonmail.ch>
2024-11-12 22:16:01 +0100

    Secrets test

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFysqYr51xIHAb53EaGG8W8uLVJXGsJ77yW3WS+mkZaH";
   linus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGavKgHzlln0r9APH/vyVQ5uGB+BXR6ybHoiAdLS+DY";
-  users = [ admin linus ];
+  vps1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAf8Wqvahm9Fm2twattmjSccCLsqpqMHrIft868NWaAd root@vps1";
+
+  users = [ admin linus vps1 ];
 in {
   "secrets.json.age".publicKeys = users;
 }
