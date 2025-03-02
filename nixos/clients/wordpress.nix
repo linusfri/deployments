2dfdd5c92d3656f270a0857a07ca185a66571417
linus <linus.f@protonmail.ch>
2025-03-02 13:45:22 +0100

    Remade wordpress service

--- nixos/clients/wordpress.nix
+++ nixos/clients/wordpress.nix
@@ -1,46 +1,62 @@
-{ config, pkgs, ... }:
+{ 
+  appName,
+  user,
+  dbName,
+  dbPrefix,
+  home,
+  assetProxy ? "" 
+}:
+{ config, pkgs, lib, ... }:
 let
+  phpFpmSettings = import ../modules/phpfpm-settings.nix;
+
   inherit (config.terraflake.input) node nodes;
 
-  lib = pkgs.lib;
-  rsync = pkgs.rsync;
-
-  appName = "wordpress";
-  user = "wordpress";
-  dbName = "wordpress";
-  serverName = "wordpress.friikod.se";
-  port = 80;
-  sslPort = 443;
-  home = "/var/lib/${user}";
+  nginxUser = config.services.nginx.user;
 
   createEnv = pkgs.writeShellScriptBin "create-env" ''
-    if [[ ! -d /var/lib/${appName} ]]; then
-      mkdir -p /var/lib/${appName}
+    if [[ ! -d ${home} ]]; then
+      mkdir -p ${home}
     fi
-  
-    cat ${config.age.secrets.wordpress-env.path} > /var/lib/${appName}/.env
+
+    cat ${config.age.secrets."${appName}-env".path} > ${home}/.env
   '';
 
   copyContentDir = pkgs.writeShellScriptBin "copy-content-dir" ''
-    CONTENT_DIR="/var/lib/${appName}/content"
+    PATH=${lib.makeBinPath [ pkgs.rsync ]}:$PATH
+    CONTENT_DIR="${home}/content"
 
-    # Do this only the first deploy
-    if [[ ! -d "$CONTENT_DIR" ]]; then
-      mkdir -p "$CONTENT_DIR"
+    mkdir -p "$CONTENT_DIR"
 
-      ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/public/content/ "$CONTENT_DIR"
+    # Sync persistant content with repo content
+    rsync -rv ${pkgs.${appName}}/share/php/${appName}/public/content/ "$CONTENT_DIR"
+    rsync -rv ${pkgs.${appName}}/share/php/${appName}/packages/themes/ "$CONTENT_DIR"/themes
+    rsync -rv ${pkgs.${appName}}/share/php/${appName}/packages/plugins/ "$CONTENT_DIR"/plugins
+
+    chown -R ${user}:${user} "$CONTENT_DIR"
+    chmod -R 755 "$CONTENT_DIR"
+  '';
+
+  setupCache = pkgs.writeShellScriptBin "set-up-cache" ''
+    PATH=${lib.makeBinPath [ pkgs.rsync ]}:$PATH
+
+    # SET UP OBJECT CACHE
+    PUBLIC_CONTENT=${pkgs.${appName}}/share/php/${appName}/public/content
+
+    # Copy object cache file to make redis work with DISALLOW_FILE_[MODS|EDIT]=true
+    # Redis plugin deletes this file if inactivated in Admin. Don't do it.
+    if [[ -d $PUBLIC_CONTENT/plugins/redis-cache ]]; then
+      rsync -vL $PUBLIC_CONTENT/plugins/redis-cache/includes/object-cache.php ${home}/content/
     fi
 
-    # Do this every following deploy after the first
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/public/content/plugins/ "$CONTENT_DIR"/plugins
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/packages/plugins/ "$CONTENT_DIR"/plugins
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/packages/themes/ "$CONTENT_DIR"/themes
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/packages/mu-plugins/ "$CONTENT_DIR"/mu-plugins
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/public/mu-plugins/ "$CONTENT_DIR"/mu-plugins
-    ${rsync}/bin/rsync -rv ${pkgs.wordpress-wp}/share/php/wordpress-wp/public/content/languages/ "$CONTENT_DIR"/languages
+    chown ${user}:${user} ${home}/content/object-cache.php
 
-    chown -R wordpress:wordpress "$CONTENT_DIR"
-    chmod -R 755 "$CONTENT_DIR"
+    # SET UP PAGE CACHE DIRECTORY FOR FASTCGI CACHE
+    mkdir -p /var/run/nginx-cache/${appName}
+    chown -R ${nginxUser}:${nginxUser} /var/run/nginx-cache/${appName}
+
+    # Don't do this. Find a better way instead of allowing all with 777
+    chmod -R 777 /var/run/nginx-cache/${appName}
   '';
 in
 {
@@ -56,10 +72,10 @@ in
   users.groups."${user}" = {
     name = user;
   };
-  
+
   services.mysql = {
-    initialDatabases = [{ name = "${dbName}"; }];
-    ensureDatabases = [dbName];
+    initialDatabases = [ { name = "${dbName}"; } ];
+    ensureDatabases = [ dbName ];
     ensureUsers = [
       {
         name = user;
@@ -70,23 +86,23 @@ in
     ];
   };
 
-  services.phpfpm.pools.${appName} = {
+  services.redis = {
+    servers = {
+      ${user} = {
+        enable = true;
+        inherit user;
+        group = user;
+        port = 0; # Listen only on unix socket
+        unixSocket = "/run/redis-${user}/redis.sock";
+      };
+    };
+  };
+
+  services.phpfpm.pools.${user} = {
     inherit user;
-    settings = {
+    settings = phpFpmSettings // {
       "listen.owner" = config.services.nginx.user;
-      "clear_env" = "no";
-      "pm" = "dynamic";
-      "pm.max_children" = 10;
-      "pm.start_servers" = 10;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 10;
-      "request_terminate_timeout" = 360;
       "access.log" = "/var/log/${user}-phpfpm-access.log";
-      "php_flag[display_errors]" = true;
-      "php_admin_value[error_log]" = "/var/log/phpfpm-error.log";
-      "php_admin_flag[log_errors]" = true;
-      "php_value[memory_limit]" = "512M";
-      "catch_workers_output" = true;
     };
     phpOptions = ''
       display_errors = on
@@ -95,31 +111,57 @@ in
     '';
     phpEnv = {
       PATH = lib.makeBinPath [ pkgs.php ];
-      ENV_FILE_PATH = "/var/lib/${appName}";
-      WP_DEBUG = "true";
-      WP_ENV = "production";
+      ENV_FILE_PATH = "/var/lib/${user}";
       WP_DEBUG_DISPLAY = "true";
-      DB_USER = "wordpress";
-      DB_NAME = "wordpress";
+      DB_USER = user;
+      DB_NAME = user;
+      DB_PREFIX = dbPrefix;
       WP_DEBUG_LOG = "/var/log/debug-wp.log";
-      WP_HOME = "https://wordpress.friikod.se";
-      WP_SITEURL = "https://wordpress.friikod.se/wp";
-      CONTENT_PATH = "/var/lib/${user}";
+      WP_HOME = "https://${node.domains.${appName}}";
+      WP_SITEURL = "https://${node.domains.${appName}}/wp";
+      CONTENT_PATH = home;
       FS_METHOD = "direct";
+      WP_REDIS_PATH = config.services.redis.servers.${user}.unixSocket;
+      WP_REDIS_PORT = "0";
+      WP_REDIS_HOST = "localhost";
+      WP_REDIS_SCHEME = "unix";
+      WP_REDIS_DISABLE_DROPIN_CHECK = "true";
+      WP_REDIS_DISABLE_DROPIN_AUTOUPDATE = "true";
     };
   };
 
-  services.phpfpm.phpOptions = ''
-    display_errors = on
-    error_log = /var/log/phpfpm-error.log
-  '';
-
   services.nginx = {
-    virtualHosts."${node.domains.wordpress-wp}" = {
+    appendHttpConfig = ''
+      fastcgi_cache_path /var/run/nginx-cache/${appName} levels=1:2 keys_zone=${appName}:100m inactive=45m;
+    '';
+    virtualHosts."${node.domains.${appName}}" = {
       enableACME = true;
       forceSSL = true;
-      root = "${pkgs.wordpress-wp}/share/php/wordpress-wp/public";
-      basicAuth = { wordpress = "wordpress"; };
+      root = "${pkgs.${appName}}/share/php/${appName}/public";
+      basicAuth = {
+        ${appName} = appName;
+      };
+      extraConfig = ''
+        set $skip_cache 0;
+
+        # POST requests and urls with a query string should always go to PHP
+        if ($request_method = POST) {
+            set $skip_cache 1;
+        }   
+        if ($query_string != "") {
+            set $skip_cache 1;
+        }   
+
+        # Don't cache uris containing the following segments
+        if ($request_uri ~* "/wp-admin/|/xmlrpc.php|wp-.*.php|/feed/|index.php|sitemap(_index)?.xml") {
+            set $skip_cache 1;
+        }   
+
+        # Don't use the cache for logged in users or recent commenters
+        if ($http_cookie ~* "comment_author|wordpress_[a-f0-9]+|wp-postpass|wordpress_no_cache|wordpress_logged_in") {
+            set $skip_cache 1;
+        }
+      '';
 
       locations."/".extraConfig = ''
         index index.php;
@@ -127,7 +169,18 @@ in
       '';
 
       locations."~ \\.php$".extraConfig = ''
-        fastcgi_pass unix:${config.services.phpfpm.pools.${appName}.socket};
+        # CACHE
+        fastcgi_cache_bypass $skip_cache;
+        fastcgi_no_cache $skip_cache;
+        fastcgi_cache ${appName};
+        fastcgi_cache_key "$scheme$request_method$host$request_uri";
+        fastcgi_cache_valid 200 301 302 30m;
+
+        # Add these headers for debugging
+        add_header X-Cache-Status $upstream_cache_status;
+
+        # PARAMS
+        fastcgi_pass unix:${config.services.phpfpm.pools.${user}.socket};
         fastcgi_index index.php;
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_param QUERY_STRING  $query_string;
@@ -152,30 +205,26 @@ in
         fastcgi_param HTTP_PROXY  "";
         fastcgi_buffer_size 512k;
         fastcgi_buffers 16 512k;
-        fastcgi_param HTTP_HOST $host;
+      '';
+
+      locations."~ /purge(/.*)".extraConfig = ''
+        fastcgi_cache_purge ${appName} "$scheme$request_method$host$1";
       '';
 
       locations."/content/uploads/".extraConfig = ''
-        alias /var/lib/${appName}/content/uploads/;
+        alias /var/lib/${user}/content/uploads/;
         try_files $uri @production;
       '';
-
-      # locations."~* \.(?:jpg|jpeg|gif|pdf|png|webp|ico|cur|gz|svg|mp4|mp3|ogg|ogv|webm|htc)$".extraConfig = ''
-      #   expires 1y;
-      #   access_log off;
-      #   add_header Access-Control-Allow-Origin *;
-      #   try_files $uri @production;
-      # '';
 
       locations."@production".extraConfig = ''
         resolver 8.8.8.8;
         proxy_ssl_server_name on;
-        proxy_pass https://www.wordpress.se;
+        proxy_pass ${assetProxy};
       '';
     };
   };
 
-  systemd.services.wordpress-env = {
+  systemd.services."${appName}-env" = {
     description = "Creates an env file";
     serviceConfig = {
       ExecStart = "${createEnv}/bin/create-env";
@@ -184,7 +233,16 @@ in
     wantedBy = [ "multi-user.target" ];
   };
 
-  systemd.services.wordpress-content-dir = {
+  systemd.services."${appName}-setupCache" = {
+    description = "Creates object-cache.php in content dir for redis to work. Sets up nginx fastcgi cache";
+    serviceConfig = {
+      ExecStart = "${setupCache}/bin/set-up-cache";
+      Type = "simple";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services."${appName}-content-dir" = {
     description = "Copies the project content folder to /var/lib";
     serviceConfig = {
       ExecStart = "${copyContentDir}/bin/copy-content-dir";
@@ -193,8 +251,18 @@ in
     wantedBy = [ "multi-user.target" ];
   };
 
-  age.secrets.wordpress-env = {
-    rekeyFile = ../${node.name}/secrets/wordpress-env.age;
+  services.borgbackup.jobs.${appName} = {
+    paths = [ "${home}/content/uploads" ];
+    encryption.mode = "none";
+    environment.BORG_RSH = "ssh -i /old-root/etc/ssh/ssh_host_ed25519_key";
+    extraCreateArgs = "--verbose --stats";
+    repo = "root@${nodes.vps1.ip}:/root/backup/${appName}";
+    compression = "auto,zstd";
+    startAt = "daily";
+  };
+
+  age.secrets."${appName}-env" = {
+    rekeyFile = ../${node.name}/secrets/${appName}-env.age;
     generator.script = "passphrase";
   };
 }
