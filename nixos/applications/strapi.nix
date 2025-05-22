9921e815ba9c7595bec06a84f2881090358c7147
linus <linus.f@protonmail.ch>
2025-05-22 19:32:56 +0200

    Updated strapi service to restrict database user permissions

--- nixos/applications/strapi.nix
+++ nixos/applications/strapi.nix
@@ -61,16 +61,13 @@ in
     ensureUsers = [
       {
         name = user;
-        ensureClauses = {
-          superuser = true;
-        };
       }
     ];
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser                 auth-method
       local all       all                    trust
-      host  all       all     ::1/128        trust
-      host  all       all     127.0.0.1/32   trust
+      host  strapi    ${user}  ::1/128       trust
+      host  strapi    ${user}  127.0.0.1/32  trust
     '';
   };
 
