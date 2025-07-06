3b4c1ad6159ab6e44ebbc5b095f5e6b749ac2857
linus <linus.f@protonmail.ch>
2025-07-06 15:40:32 +0200

    Migration progress

---
+++ infrastructure/friikod/server.tf
@@ -0,0 +1,19 @@
+resource "hcloud_ssh_key" "main" {
+  name       = "main_ssh"
+  public_key = var.ssh_pub
+}
+
+resource "hcloud_server" "nixos" {
+  name        = "hetzvps"
+  image       = "ubuntu-22.04"
+  server_type = "cpx31"
+  datacenter  = "hel1-dc2"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  ssh_keys = [hcloud_ssh_key.main.id]
+  labels = {
+    "label" : "seed"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,62 +1,17 @@
-variable "ssh_pub" {}
 variable "cloudflare_id" {}
+variable "cloudflare_token" {}
+variable "ssh_pub" {}
 
-module "hetzvps" {
-  source = "./modules/server/hetzner/vps"
-  name = "hetzvps"
-  ssh_key = var.ssh_pub
-  image = "ubuntu-22.04"
-  server_type = "cpx31"
-  label = "seed"
-
-  domains        = {
-    "friikod" = "friikod.se",
-    "ladugardlive" = "ladugardlive.se",
-    "calc-api" = "calc.friikod.se"
-    "auth-server" = "auth-server.friikod.se"
-    "strapi" = "strapi.friikod.se"
-    "next" = "next.friikod.se"
-    "plex" = "plex.friikod.se"
-  }
-}
-
-module "dns_friikod_se" {
-  source     = "./modules/dns"
-  account_id = var.cloudflare_id
-  domain     = "friikod.se"
-  subdomains = {
-    "www" = module.hetzvps.node.ip
-    "calc" = module.hetzvps.node.ip
-    "auth-server" = module.hetzvps.node.ip
-    "strapi" = module.hetzvps.node.ip
-    "next" = module.hetzvps.node.ip
-    "plex" = module.hetzvps.node.ip
-  }
-  ip = module.hetzvps.node.ip
-  # ip6 = module.server.nodes.ip6
-}
-
-module "dns_ladugard_se" {
-  source     = "./modules/dns"
-  account_id = var.cloudflare_id
-  domain     = "ladugardlive.se"
-  subdomains = {
-    "www" = module.hetzvps.node.ip
-  }
-  ip = module.hetzvps.node.ip
-  # ip6 = module.server.nodes.ip6
-}
-
-# Add DNS servers to opentofu output.
-output "ns" {
-  value = {
-    "${module.hetzvps.node.domains.friikod}" = module.dns_friikod_se.ns,
-    "${module.hetzvps.node.domains.ladugardlive}" = module.dns_ladugard_se.ns
-  }
+module "friikod" {
+  source = "./infrastructure/friikod"
+  cloudflare_id = var.cloudflare_id
+  cloudflare_token = var.cloudflare_token
+  ssh_pub = var.ssh_pub
 }
 
 # Add all node information to output for `terraflake`.
 output "terraflake" {
-  # value = [module.node1.node, module.node2.node]
-  value = [module.hetzvps.node]
+  value = [
+    module.friikod.nodes.friikod
+  ]
 }
