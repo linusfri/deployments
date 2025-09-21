ece7417d51a80929ce4a840f8ef5338cd3a95df3
linus <linus.f@protonmail.ch>
2025-09-21 17:05:05 +0200

    Updates and properly configured keycloak

--- nixos/applications/keycloak.nix
+++ nixos/applications/keycloak.nix
@@ -6,6 +6,7 @@
 }:
 let
   inherit (config.terraflake.input) node;
+  domain = node.domains.keycloak;
 in
 {
   services.keycloak = {
@@ -13,10 +14,30 @@ in
     database = {
       passwordFile = config.age.secrets.keycloakDbPassFile.path;
     };
+    sslCertificate = "${config.security.acme.certs.${domain}.directory}/cert.pem";
+    sslCertificateKey = "${config.security.acme.certs.${domain}.directory}/key.pem";
     settings = {
       hostname = "keycloak.friikod.se";
       http-port = 9090;
       https-port = 9443;
+      proxy-headers = "xforwarded";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    keycloak
+  ];
+
+  services.nginx.virtualHosts."${domain}" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "https://localhost:9443";
+      extraConfig = ''
+        proxy_set_header X-Forwarded-For $proxy_protocol_addr;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+      '';
     };
   };
 
