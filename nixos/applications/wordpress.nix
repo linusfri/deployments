70462ee988aafd03442516c9049725e203b2e0ed
linus <linus.f@protonmail.ch>
2025-10-07 21:38:07 +0200

    Removed multisite wordpress

--- nixos/applications/wordpress.nix
+++ nixos/applications/wordpress.nix
@@ -7,68 +7,7 @@ let
   inherit (config.terraflake.input) node;
 in
 {
-  # imports = [ ../modules/wordpress/wordpress-multisite-service.nix ];
   imports = [ ../modules/wordpress-service.nix ];
-
-  # services.linusfri.wordpress.sites = {
-  #   # First site
-  #   elin = {
-  #     package = pkgs.bedrock-wp;
-  #     user = "elin";
-  #     home = "/var/lib/elin";
-  #     domain = node.domains.elin;
-  #     dbName = "elin_db";
-  #     dbPrefix = "wp_";
-  #     projectDir = "bedrock-wp";
-
-  #     environment = "production";
-
-  #     debug = {
-  #       enabled = false;
-  #       display = false;
-  #     };
-
-  #     ssl = {
-  #       enable = true;
-  #       force = true;
-  #     };
-
-  #     basicAuth = {
-  #       enable = false;
-  #     };
-
-  #     assetProxy = "";
-  #   };
-
-  #   # Second site
-  #   mysite = {
-  #     package = pkgs.bedrock-wp;
-  #     user = "carolin";
-  #     home = "/var/lib/carolin";
-  #     domain = "carolin.example.com";
-  #     dbName = "carolin_db";
-  #     dbPrefix = "wp_";
-  #     projectDir = "wordpress";
-
-  #     environment = "production";
-
-  #     debug = {
-  #       enabled = true;
-  #       display = true;
-  #     };
-
-  #     ssl = {
-  #       enable = false;
-  #       force = false;
-  #     };
-
-  #     basicAuth = {
-  #       enable = true; # Enable basic auth for staging
-  #     };
-  #   };
-
-  #   # Add more sites as needed...
-  # };
 
   services.linusfri.wordpress = {
     enable = true;
