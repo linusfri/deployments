07c0ecc676579ccf3fa8afb6e6d9aefb84368bad
linus <linus.f@protonmail.ch>
2025-11-29 19:08:08 +0100

    Tofu formatting

--- infrastructure/friikod/main.tf
+++ infrastructure/friikod/main.tf
@@ -8,17 +8,17 @@ output "nodes" {
       ssh_key  = var.ssh_pub
       label    = "friikod"
       domains = {
-        "friikod"      = "friikod.se"
-        "ladugardlive" = "ladugardlive.se"
-        "calc-api"     = "calc.friikod.se"
-        "auth-server"  = "auth-server.friikod.se"
-        "strapi"       = "strapi.friikod.se"
-        "next"         = "next.friikod.se"
-        "plex"         = "plex.friikod.se"
-        "nextcloud"    = "nextcloud.friikod.se"
-        "jellyfin"     = "jellyfin.friikod.se"
-        "keycloak"     = "keycloak.friikod.se"
-        "elin"         = "elin.friikod.se"
+        "friikod"          = "friikod.se"
+        "ladugardlive"     = "ladugardlive.se"
+        "calc-api"         = "calc.friikod.se"
+        "auth-server"      = "auth-server.friikod.se"
+        "strapi"           = "strapi.friikod.se"
+        "next"             = "next.friikod.se"
+        "plex"             = "plex.friikod.se"
+        "nextcloud"        = "nextcloud.friikod.se"
+        "jellyfin"         = "jellyfin.friikod.se"
+        "keycloak"         = "keycloak.friikod.se"
+        "elin"             = "elin.friikod.se"
         "ladugardlive-web" = "web.ladugardlive.se"
       }
     }
