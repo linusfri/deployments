afa2687d1cca44018ee495fea13fbb5b588929f0
linus <linus.f@protonmail.ch>
2026-02-04 19:16:34 +0100

    Readded elin site

--- nixos/applications/wordpress.nix
+++ nixos/applications/wordpress.nix
@@ -10,13 +10,39 @@ in
   imports = [ ../modules/wordpress/wordpress-service.nix ];
 
   services.linusfri.wordpress.sites = {
-    ladugard = {
-      appName = "ladugard";
+    # ladugard = {
+    #   appName = "ladugard";
+    #   package = pkgs.bedrock-wp; # Replace with your actual package
+    #   user = "ladugard";
+    #   home = "/var/lib/ladugard";
+    #   domain = node.domains.ladugardlive-web; # Or use a string like "example.com"
+    #   dbName = "ladugard_db";
+    #   dbPrefix = "wp_";
+    #   projectDir = "bedrock-wp"; # Adjust based on your package structure
+
+    #   environment = "production";
+
+    #   debug = {
+    #     enabled = false;
+    #     display = false;
+    #   };
+
+    #   ssl = {
+    #     enable = true;
+    #     force = true;
+    #   };
+
+    #   basicAuth = {
+    #     enable = false;
+    #   };
+    # };
+    elin = {
+      appName = "elin";
       package = pkgs.bedrock-wp; # Replace with your actual package
-      user = "ladugard";
-      home = "/var/lib/ladugard";
-      domain = node.domains.ladugardlive-web; # Or use a string like "example.com"
-      dbName = "ladugard_db";
+      user = "elin";
+      home = "/var/lib/elin";
+      domain = node.domains.elin; # Or use a string like "example.com"
+      dbName = "elin_db";
       dbPrefix = "wp_";
       projectDir = "bedrock-wp"; # Adjust based on your package structure
 
@@ -36,5 +62,6 @@ in
         enable = false;
       };
     };
+
   };
 }
